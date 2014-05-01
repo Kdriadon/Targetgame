2b3755a3983e680bdb81a7347f80053add183b42
Kevin Driadon <kad367@drexel.edu>
2014-05-01 13:55:06 -0400

    Mode menu

--- Target/src/Game.java
+++ Target/src/Game.java
@@ -2,6 +2,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -11,13 +12,43 @@ public class Game extends JFrame implements ActionListener
 	public Game()
 	{
 		super ("Target Game"); 
-		setLayout(new FlowLayout()); 
-		JPanel panel = new JPanel(); 
-	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+		
+		
+	}
+	public void Mode(){
+		JPanel Mode;
+		super("Game Type");
+		setLayout(new FlowLayout());
+		JButton Normal = new JButton("Normal");
+		Normal.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						playNormalGame();
+					}
+				}
+				);
+		JButton SpeedRun = new JButton("Speed Run");
+		SpeedRun.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						playSpeedGame();
+					}
+				}
+				);
+		JButton Return = new JButton ("Return");
+		Return.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						mainMenu();
+					}
+				}
+				);
+		 Mode = new JPanel();
+		
+		Mode.add(Normal);
+		Mode.add(SpeedRun);
+		Mode.add(Return);
 	}
 }
