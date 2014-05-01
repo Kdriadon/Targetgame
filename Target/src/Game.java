6a97d61dc10ed81884a5717d986c0fc0d866c63c
Christopher Macco <crm97@tux.cs.drexel.edu>
2014-05-01 13:23:22 -0400

    major layout changes

--- Target/src/Game.java
+++ Target/src/Game.java
@@ -1,4 +1,23 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Game {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+
+public class Game extends JFrame implements ActionListener
+{
+	public Game()
+	{
+		super ("Target Game"); 
+		setLayout(new FlowLayout()); 
+		JPanel panel = new JPanel(); 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- Target/src/SettingsMenu.java
+++ Target/src/SettingsMenu.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 public class SettingsMenu 
 {
-	private JRadioButton easy, meduim, hard; 
+	private JRadioButton easy, medium, hard; 
 	private JRadioButton black, blue, green, yellow, white; 
 	private JRadioButton speedRun, normal; 
 }
