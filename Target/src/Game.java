03777350b891adc84fe6abee494d289286bbd184
at9418 <at672@drexel.edu>
2014-05-15 13:16:14 -0400

    Update Game.java

--- Target/src/Game.java
+++ Target/src/Game.java
@@ -1,17 +1,49 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Game extends JFrame
-{
+import javax.swing.*;
+public class Game extends JButton implements ActionListener   {
+	public Game(){
+		
+		target();
+	}
+	public void target(){
+		JFrame frame = new JFrame("HIT!");
+		frame.setVisible(true);
+		frame.setSize(600,400);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
 	
-	public Game()
-	{
+		JButton  first = new JButton();
+		first.setBackground(Color.WHITE);
+		first.setSize(75, 75);
+		JButton second = new JButton();
+		second.setBackground(Color.RED);
+		second.setSize(50, 50);
+
+		JButton third = new JButton();
+		third.setBackground(Color.WHITE);
+		third.setSize(25, 25);
+
+		panel.add(first);
+		first.add(second);
+		second.add(third);
+		
+		
+		this.setVisible(true);
+		first.addActionListener(this);
+		second.addActionListener(this);
+		third.addActionListener(this);
+		 
+		frame.add(panel);
+
 		
 	}
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	public static void  main(String args[]) {
+		new Game();
+}
 }
