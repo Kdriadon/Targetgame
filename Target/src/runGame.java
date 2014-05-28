767c5ba59cd661370c5c248b37c319da7bbbc062
Chris <crm97@drexel.edu>
2014-05-28 18:42:37 -0400

    test commit

--- Target/src/runGame.java
+++ Target/src/runGame.java
@@ -5,19 +5,22 @@ public class runGame
 {
 	public static void main(String[] agrs)
 	{
+		readAndApplySettings(); 
+		
 		MainMenu application = new MainMenu(); 
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		application.setSize(400, 300);
-		application.setVisible(true); 
+		application.setVisible(true);  
+	}
+	public static void readAndApplySettings()
+	{
+		readIn retrieve = new readIn(); 
+		retrieve.openFile();
+		retrieve.readInSettings();
+		retrieve.getColor();
+		retrieve.closeReadFile();
 		
-		
-//		readIn retrieve = new readIn(); 
-//		retrieve.openFile();
-//		retrieve.readInSettings();
-//		retrieve.closeReadFile();
-//		
-//		applySettings settings = new applySettings(); 
-//		settings.applyToSettings();
-		//This line above should be called in the regular menus so we can control the visibility of the menus 
+		applySettings settings = new applySettings(); 
+		settings.applyToSettings();
 	}
 }
