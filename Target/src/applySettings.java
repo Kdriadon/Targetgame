16773ae5ade574e66fe392ab1df12c77711d74b4
cmcode97 <crm97@drexel.edu>
2014-05-28 18:26:55 -0400

    Update applySettings.java
    updating apply settings

--- Target/src/applySettings.java
+++ Target/src/applySettings.java
@@ -1,77 +1,53 @@
+import java.awt.Color;
 
-public class applySettings extends SettingsMenu
+
+public class applySettings extends readIn
 {
+	//for the settings menu
 	public void applyToSettings()
 	{
 		if (Easy == true)
 		{
 			easy.setSelected(true); 
-			medium.setSelected(false);
-			hard.setSelected(false); 
 		}
 		else if (Medium == true)
 		{
-			easy.setSelected(false); 
 			medium.setSelected(true);
-			hard.setSelected(false);
 		}
 		else if (Hard == true)
 		{
-			easy.setSelected(false); 
-			medium.setSelected(false);
 			hard.setSelected(true);
 		}
 		if (Black == true)
 		{
 			black.setSelected(true);
-			blue.setSelected(false);
-			green.setSelected(false); 
-			yellow.setSelected(false); 
-			white.setSelected(false); 
 		}
 		else if (Blue == true)
 		{
-			black.setSelected(false);
 			blue.setSelected(true);
-			green.setSelected(false); 
-			yellow.setSelected(false); 
-			white.setSelected(false); 
 		}
 		else if (Green == true)
 		{
-			black.setSelected(false);
-			blue.setSelected(false);
 			green.setSelected(true); 
-			yellow.setSelected(false); 
-			white.setSelected(false);
 		}
 		else if (Yellow == true)
-		{
-			black.setSelected(false);
-			blue.setSelected(false);
-			green.setSelected(false); 
+		{ 
 			yellow.setSelected(true); 
-			white.setSelected(false); 
 		}
 		else if (White == true)
 		{
-			black.setSelected(false);
-			blue.setSelected(false);
-			green.setSelected(false); 
-			yellow.setSelected(false); 
 			white.setSelected(true); 
 		}
 		if (Normal == true)
 		{
-			speedRun.setSelected(false); 
 			normal.setSelected(true);
 		}
 		else if (SpeedRun == true)
 		{
 			speedRun.setSelected(true); 
-			normal.setSelected(false);
 		}
 	}
+	//for the other classes 
 	public void applyToOthers()
 	{
 		if (Easy == true)
@@ -88,7 +64,8 @@ public class applySettings extends SettingsMenu
 		}
 		if (Black == true)
 		{
-			
+			panelSettings.setBackground(Color.BLACK);
+			panelSettings.repaint();
 		}
 		else if (Blue == true)
 		{
@@ -112,7 +89,7 @@ public class applySettings extends SettingsMenu
 		}
 		else if (SpeedRun == true)
 		{
-			
+			//Not made
 		}
 	}
 }
