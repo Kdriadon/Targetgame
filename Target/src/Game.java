096990ab95be97adc386d247d74616c6f0894d81
Kevin Driadon <kad367@drexel.edu>
2014-05-28 18:16:50 -0400

    Implemented Timer

--- Target/src/Game.java
+++ Target/src/Game.java
@@ -1,14 +1,56 @@
-
+import java.util.Timer;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
+import java.util.TimerTask;
 
 import javax.swing.*;
 public class Game extends JButton   {
+	int secs = 10000;
+	int minus = 500;
+	int stops = 1000;
+	
+	public Timer timer; 
+
 	public Game(){
 		
+	}
+	public void runGame()
+	{
+		time();
 		target();
 	}
+	public void time(){
+		
+		 
+		
+		{
+			TimerTask gameover = new TimerTask() { 
+	            @Override
+	            public void run() {
+	                JOptionPane.showMessageDialog(null, "You lost the game!","Game Over",JOptionPane.ERROR_MESSAGE);
+	                System.exit(0);
+	            }
+	         };
+	         timer = new Timer(); 
+	         timer.schedule(gameover, secs);
+		}
+	}
+	public void updateTime(int seconds)
+	{
+		TimerTask gameover = new TimerTask()
+		{
+			@Override
+			public void run() {
+                JOptionPane.showMessageDialog(null, "You lost the game!","Game Over",JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+			}	
+		}; 
+		timer.cancel();
+		timer = new Timer(); 
+		timer.schedule(gameover, seconds);
+	}
+	
 	//initialize the values
 	int fpoint = 0;
 	int spoint = 0;
@@ -38,6 +80,10 @@ public class Game extends JButton   {
 		second.add(third);
 		
 		this.setVisible(true);
+		
+
+		
+		
 		//action listener for the first button
 		first.addActionListener(
 				new ActionListener()
@@ -48,9 +94,14 @@ public class Game extends JButton   {
 							 total = fpoint + spoint + tpoint; 
 							 System.out.println(total);
 							 if (e.getSource() == first){
+								 
 							        first.setLocation(rand.nextInt(300),rand.nextInt(300));
 					                first.repaint();
 					            }
+				                if (secs-500 >= 1000){
+					                secs -= 500;}
+					                
+					                updateTime(secs);  
 					}
 					}
 				
@@ -72,8 +123,12 @@ public class Game extends JButton   {
 						                first.setLocation(rand.nextInt(300),rand.nextInt(300));
 						                first.repaint();
 						            }
+					                if (secs-500 >= 1000){
+						                secs -= 500;}
+						                
+						                updateTime(secs);  
 						}
-							 }
+					}
 					
 				}
 			);
@@ -92,8 +147,12 @@ public class Game extends JButton   {
 							 for(int i=0;i<=300;i++)
 					            {
 					                first.setLocation(rand.nextInt(300),rand.nextInt(300));
-					                first.repaint();
+					                first.repaint();  
 					            }
+				                if (secs-500 >= 1000){
+					                secs -= 500;}
+					                
+					                updateTime(secs);  
 					}
 						 }
 					
@@ -105,7 +164,9 @@ public class Game extends JButton   {
 		frame.add(panel);
 	}
 	//the main method	
-	public static void  main(String args[]) {
-		new Game();
+
+	public static void  main(String args[]) { 
+		Game g = new Game(); 
+		g.runGame();
 }
 }
